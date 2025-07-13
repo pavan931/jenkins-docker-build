aa27d3479a1ef7df676836eb314d79c1e71e0cea
Ubuntu <pavand305@gmail.com>
2025-07-13 13:57:23 +0000

    basic react-docker-jenkins

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '40px' }}>
+      <h1>Hello from React inside Docker! 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
+
